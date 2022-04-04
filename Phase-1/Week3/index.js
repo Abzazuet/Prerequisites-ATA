2352ea5a6ac131d5ee0288a34845ca3a2bfd38c1
Abel Zazueta <abel.zazueta@gmail.com>
2022-04-04 11:05:48 -0700

    Canvas fished

--- Phase-1/Week3/index.js
+++ Phase-1/Week3/index.js
@@ -1,23 +1,25 @@
-function doRed() {
-    let dd1 = document.getElementById("div1");
-    dd1.className = "redback"
-    let ctx = dd1.getContext("2d");
-    ctx.fillStyle = "yellow";
-    ctx.fillRect(10, 10, 60, 60);
-    ctx.fillRect(80, 10, 60, 60);
+let dd1 = document.getElementById("div1");
+let ctx = dd1.getContext("2d");
 
+function doRed() {
+    ctx.clearRect(0, 0, dd1.width,dd1.height);
+    dd1.style.backgroundColor = "red";
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(10, 10, 40, 40);
+    ctx.fillRect(60, 10, 40, 40);
+    ctx.font = ("15px Arial");
     ctx.fillStyle = "black";
-    ctx.font = "20px Arial";
-    ctx.fillText = ("Hello", 0, 0);
+    ctx.fillText("Hello", 30, 30);
+
 }
 function doBlue() {
-    let dd1 = document.getElementById("div1");
-    dd1.className = "blueback";
-    let ctx = dd1.getContext("2d");
+    ctx.clearRect(0, 0, dd1.width,dd1.height);
+    dd1.style.backgroundColor = "blue";
     ctx.fillStyle = "yellow";
-    ctx.fillRect(10, 10, 60, 60);
-    ctx.fillRect(80, 10, 60, 60);
-
-    ctx.font = "20000px Arial";
-    ctx.fillText = ("Hello", 0, 0);
+    ctx.fillRect(10, 10, 40, 40);
+    ctx.fillRect(60, 10, 40, 40);
+    
+    ctx.font = ("15px Arial");
+    ctx.fillStyle = "black";
+    ctx.fillText("Hello", 10, 30);
 }
