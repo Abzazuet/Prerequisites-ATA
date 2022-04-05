878893ab7e2f7e70ace3da8d72e2f2772ed033fd
Abel Zazueta <abel.zazueta@gmail.com>
2022-04-04 17:28:32 -0700

    Paint Canvas finished

--- Phase-1/Week3/PaintCanvas/index.js
+++ Phase-1/Week3/PaintCanvas/index.js
@@ -2,13 +2,39 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let width = document.getElementById("width");
 let height = document.getElementById("height");
-
-
-function setWidth(){
+let isPainting = false;
+let color = document.getElementById("colorPicker").value;
+let radius = document.getElementById("brushSize").value;
+let output = document.getElementById("sizeOutput");
+function setWidth() {
     canvas.width = width.value;
-    console.log(canvas.width)
 }
-function setHeight(){
+function setHeight() {
     canvas.height = height.value;
-    console.log(canvas.height)
 }
+function changeColor() {
+    color = document.getElementById("colorPicker").value;
+}
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function changeBrushSize() {
+    output.value = radius;
+    radius = document.getElementById("brushSize").value;
+}
+function startPaint() {
+    isPainting = true;
+}
+function endPaint() {
+    isPainting = false;
+}
+function doPaint(x, y) {
+    if (isPainting) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+}
+
+
