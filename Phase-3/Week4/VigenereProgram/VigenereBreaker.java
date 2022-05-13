d264d5c5a24b1961f60d012762bcfc1b88635e45
Abel Zazueta <abel.zazueta@gmail.com>
2022-05-13 16:40:48 -0700

    Unkown key length

--- Phase-3/Week4/VigenereProgram/VigenereBreaker.java
+++ Phase-3/Week4/VigenereProgram/VigenereBreaker.java
@@ -3,10 +3,10 @@ import edu.duke.*;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
-        //REPLACE WITH YOUR CODE
+        // REPLACE WITH YOUR CODE
         int totalLength = message.length();
         StringBuilder slicedString = new StringBuilder();
-        for (int i = whichSlice; i<totalLength;i=i+totalSlices){
+        for (int i = whichSlice; i < totalLength; i = i + totalSlices) {
             slicedString.append(message.charAt(i));
         }
         return slicedString.toString();
@@ -14,31 +14,67 @@ public class VigenereBreaker {
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
-        //WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
         CaesarCracker decrypter = new CaesarCracker();
-        String [] slices = new String[klength];
-        for (int i =0; i<klength; i++){
+        String[] slices = new String[klength];
+        for (int i = 0; i < klength; i++) {
             slices[i] = sliceString(encrypted, i, klength);
             key[i] = decrypter.getKey(slices[i]);
         }
         /*
-        for (int i : key){
-            System.out.println(i);
-        }
-        */
+         * for (int i : key){
+         * System.out.println(i);
+         * }
+         */
         return key;
     }
 
-    public void breakVigenere () {
-        //WRITE YOUR CODE HERE
-        FileResource file = new FileResource();
-        String text = file.asString();
-        int keys [] = tryKeyLength(text, 4, 'e');
-        VigenereCipher cracker = new VigenereCipher(keys);
-        String messageDecrypted = cracker.decrypt(text);
-        System.out.println(messageDecrypted);
+    public HashSet<String> readDictionary(FileResource fr) {
+        HashSet<String> words = new HashSet<String>();
+        for (String line : fr.lines()) {
+            words.add(line.toLowerCase());
+        }
+        return words;
     }
-     
+
+    public int countDictionary(String message, HashSet<String> dictionary) {
+        int wordsFound = 0;
+        message = message.toLowerCase();
+        String[] wordsInMessage = message.split("\\W+");
+        for (String word : wordsInMessage) {
+            if (dictionary.contains(word)) {
+                wordsFound++;
+            }
+        }
+        return wordsFound;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary) {
+        String message = "";
+        int max = 0;
+        int [] keys = new int[10];
+        for (int i = 1; i < 101; i++) {
+             keys = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher cracker = new VigenereCipher(keys);
+            String messageDecrypted = cracker.decrypt(encrypted);
+            int wordsInDictionary = countDictionary(messageDecrypted, dictionary);
+            if(wordsInDictionary>max){
+                max = wordsInDictionary;
+                message = messageDecrypted;
+            }
+        }
+        return message;
+    }
+
+    public void breakVigenere() {
+        // WRITE YOUR CODE HERE
+        FileResource file = new FileResource();
+        HashSet<String> dictionary = readDictionary(new FileResource("./dictionaries/English"));      
+        String message = file.asString();
+        String messageDecrypted = breakForLanguage(message, dictionary);
+        System.out.print(messageDecrypted);
+    }
+
     public static void main(String[] args) {
         VigenereBreaker test = new VigenereBreaker();
         test.breakVigenere();
